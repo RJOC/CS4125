65a4e4110ef6e7739a689ce98accd593357cba05
RJOC <34495523+RJOC@users.noreply.github.com>
2020-11-30 20:41:40 +0000

    Java_Skeleton
    Pleasework.exe

---
+++ Java_Code/DanceManagerStudioClient/src/dancemanagerstudioclient/DMSRegFrame.java
@@ -0,0 +1,192 @@
+/*
+ * This is the registration screen to create a new account
+ */
+package dancemanagerstudioclient;
+
+//import ttt.james.server.TTTWebService;
+//import ttt.james.server.TTTWebService_Service;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author RJ
+ */
+public class DMSRegFrame extends JFrame implements ActionListener {
+    //Webserver initialisations
+    //private TTTWebService proxy;
+    //private TTTWebService_Service ttt;
+    //To call the last frame
+    private DanceManagerStudioClient parent;
+    private JTextField name, surname, email, password;
+    private JLabel heading,nameLab, surnameLab, emailLab, passLab;
+    private JButton back, submit, clear;
+    private DanceManagerStudioClient menu;
+    
+
+    public DMSRegFrame(DanceManagerStudioClient dad){
+        parent = dad;
+        //ttt = new TTTWebService_Service();
+        //proxy = ttt.getTTTWebServicePort();
+        
+        //Frame Setup
+        setTitle("Reistration");
+        setLayout(new BorderLayout());
+        
+        
+        //Section 2
+        JPanel sec2 = new JPanel();
+        sec2.setLayout(new GridLayout(9,1));
+        nameLab = new JLabel("Name: ",JLabel.CENTER);
+        nameLab.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 25));
+        name = new JTextField();
+        name.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 20));
+        name.setHorizontalAlignment(JTextField.CENTER);
+        sec2.add(nameLab);
+        sec2.add(name);
+        surnameLab = new JLabel("Surname: ",JLabel.CENTER);
+        surnameLab.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 25));
+        surname = new JTextField();
+        surname.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 20));
+        surname.setHorizontalAlignment(JTextField.CENTER);
+        sec2.add(surnameLab);
+        sec2.add(surname);
+        emailLab = new JLabel("Email: ",JLabel.CENTER);
+        emailLab.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 25));
+        email = new JTextField();
+        email.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 20));
+        email.setHorizontalAlignment(JTextField.CENTER);
+        sec2.add(emailLab);
+        sec2.add(email);
+        passLab = new JLabel("Password: ",JLabel.CENTER);
+        passLab.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 25));
+        password = new JTextField();
+        password.setHorizontalAlignment(JTextField.CENTER);
+        password.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 20));
+        sec2.add(passLab);
+        sec2.add(password);
+        
+        
+        //Section 3
+        JPanel sec3 = new JPanel();
+        sec3.setLayout(new GridLayout(1,3));
+        back = new JButton("Back");
+        back.addActionListener(this);
+        sec3.add(back);
+        clear = new JButton("Clear");
+        clear.addActionListener(this);
+        sec3.add(clear);
+        submit = new JButton("Submit");
+        submit.addActionListener(this);
+        sec3.add(submit);
+        
+         //JFRAME LAYOUT
+        getContentPane().add(sec2,BorderLayout.CENTER);
+        getContentPane().add(sec3,BorderLayout.SOUTH);
+        setSize(650,500);
+        setVisible(true);
+        
+        
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        WindowListener exitListener = new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent e){
+                parent.setVisible(true);
+                dispose();
+            }
+            
+        };
+        addWindowListener(exitListener);
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if(source == back){
+            parent.setVisible(true);
+            dispose();
+        }
+        if(source == clear){
+            email.setText("");
+            password.setText("");
+        }
+        if(source == submit){
+            String nam = name.getText();
+            String sur = surname.getText();
+            String usr = email.getText();
+            String pas = password.getText();
+            
+            if(nam.equals("")){
+                JOptionPane.showMessageDialog(null, "No Name Entered");
+            } else if (sur.equals("")) {
+                JOptionPane.showMessageDialog(null, "No Surname Entered");
+            } else if(usr.equals("")) {
+                JOptionPane.showMessageDialog(null, "No email Entered");
+            } else if(pas.equals("")) {
+                JOptionPane.showMessageDialog(null, "No password Entered");
+            } else {
+                //String value = proxy.register(usr, pas, nam, sur); Fix this when the proxy is setup
+                
+                switch(""){ //The variable "Value" needs to be put here
+                case "ERROR-REPEAT":
+                    JOptionPane.showMessageDialog(null, "The email already exists");
+                    email.setText("");
+                    password.setText("");
+                    email.requestFocusInWindow();
+                    break;
+                    
+                case "ERROR-INSERT":
+                    JOptionPane.showMessageDialog(null, "Could not add data to user table!");
+                    email.setText("");
+                    password.setText("");
+                    email.requestFocusInWindow();
+                    break;
+                    
+                case "ERROR-RETRIEVE":
+                    JOptionPane.showMessageDialog(null, "Could not retrieve the newly inserted data from the users table!");
+                    email.setText("");
+                    password.setText("");
+                    email.requestFocusInWindow();
+                    break;
+                    
+                case "ERROR-DB":
+                    JOptionPane.showMessageDialog(null, "Could not find the data base!");
+                    email.setText("");
+                    password.setText("");
+                    email.requestFocusInWindow();
+                    break;
+                    
+                default:
+                    name.setText("");
+                    surname.setText("");
+                    email.setText("");
+                    password.setText("");
+                    email.requestFocusInWindow();
+                    setVisible(false);
+                    //Here we need to state to go to a new
+                    //int pid = Integer.parseInt(value);
+                    DMSMenuFrame menu = new DMSMenuFrame(pid, parent, usr);
+
+                }
+            }
+            
+            
+            
+        }
+    }
+    
+}
---
+++ Java_Code/DanceManagerStudioClient/src/dancemanagerstudioclient/DanceManagerStudioClient.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dancemanagerstudioclient;
+
+
+//import ttt.james.server.TTTWebService;
+//import ttt.james.server.TTTWebService_Service;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import java.awt.GridLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+
+
+/**
+ * Start Date: 09/11/2019
+ * @author Ryan O'Connor
+ */
+public class DanceManagerStudioClient extends JFrame implements ActionListener{
+        //Webserver initialisations
+    //private TTTWebService proxy;
+    //private TTTWebService_Service ttt;
+    //JButton initialisations because they are referenced in this whole class
+    private JButton regBttn , loginBttn;
+   
+    public DanceManagerStudioClient() throws IOException{
+        //ttt = new TTTWebService_Service();
+        //proxy = ttt.getTTTWebServicePort();
+        
+        //Frame Setup
+        setTitle("The Ani Mje  Special");
+        setLayout(new BorderLayout());
+        
+        
+        //Section 1
+            JPanel sec1 = new JPanel();
+            sec1.setLayout(new BorderLayout());
+            JLabel heading = new JLabel("Dance Manager Studio",JLabel.CENTER);
+            JLabel des = new JLabel("This software is brought to you by The Ani Mje Special!", JLabel.CENTER);
+            heading.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 45));
+            des.setVerticalAlignment(JLabel.TOP);
+            des.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+            sec1.add(heading, BorderLayout.NORTH);
+            sec1.add(des,BorderLayout.CENTER);
+            
+        //Section 2
+            JPanel sec2 = new JPanel();
+            BufferedImage Pic = ImageIO.read(this.getClass().getResource("dance.png"));
+            JLabel Icon = new JLabel(new ImageIcon(Pic));
+            sec2.add(Icon);
+            sec2.setSize(400,400);
+            
+        //Section 3
+            JPanel sec3 = new JPanel();
+            sec3.setLayout(new GridLayout(1,2));
+            loginBttn = new JButton("Login");
+            loginBttn.addActionListener(this);
+            regBttn = new JButton("Register");
+            regBttn.addActionListener(this);
+            regBttn.setSize(50,50);
+            sec3.add(regBttn);
+            sec3.add(loginBttn);
+
+                
+        //JFRAME LAYOUT
+        getContentPane().add(sec1,BorderLayout.NORTH);
+        getContentPane().add(sec2,BorderLayout.CENTER);
+        getContentPane().add(sec3,BorderLayout.SOUTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(650,500);
+        setVisible(true);
+       
+        
+        //sec1.setBackground(Color.getHSBColor(181, 18, 100));
+    }
+
+    
+    /**
+     * @param args the command line arguments
+     * Main method:
+     *      Displays the welcome screen of the game
+     *      Allows you to select if you want to login or register.
+     */
+    public static void main(String[] args) throws IOException {
+
+        DanceManagerStudioClient myInstance = new DanceManagerStudioClient();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try{
+            Object source = e.getSource();
+            if(source == loginBttn){
+                setVisible(false);
+                //TTTLoginFrame login = new TTTLoginFrame(this);
+            }
+            if(source == regBttn){
+                setVisible(false);
+                DMSRegFrame login = new DMSRegFrame(this);
+            }
+        }catch(Exception ee){
+            System.err.print(ee.getMessage());
+        }
+    }
+
+    
+}
+
