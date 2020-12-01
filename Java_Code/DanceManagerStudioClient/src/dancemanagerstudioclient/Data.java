db252a30391d8430c6b20418f723df101fdf54fd
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2020-12-01 10:37:13 +0000

    Templates
    Template for Calling Web Service, Will link to Data factory methods

---
+++ Java_Code/DanceManagerStudioClient/src/dancemanagerstudioclient/Data.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dancemanagerstudioclient;
+import java.util.ArrayList; // import the ArrayList class
+
+/**
+ *
+ * @author Jono
+ */
+public class Data {
+    private ArrayList<ArrayList<String>> data;
+
+    public Data() {
+        this.data = new ArrayList<ArrayList<String>>();
+    }
+}
