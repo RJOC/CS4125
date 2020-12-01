383e70f7770d57c59390039c2e52409ba187e4b6
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2020-12-01 12:38:00 +0000

    Create DanceClass.java

---
+++ Java_Code/DanceManagerStudioClient/src/dancemanagerstudioclient/DanceClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dancemanagerstudioclient;
+import java.util.ArrayList;
+/**
+ *
+ * @author Jono
+ */
+public class DanceClass{
+    private ArrayList<ArrayList<String>> studentList;
+    private String teacherName;
+    private int teacherID;
+    ///TODO: add rest of relevant fields and Gen get/set
+    
+}
