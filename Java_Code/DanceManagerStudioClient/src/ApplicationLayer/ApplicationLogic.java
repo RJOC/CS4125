7bdb619d3b4fb2feb65e6fd43963f0329f005414
RJOC <34495523+RJOC@users.noreply.github.com>
2020-12-11 17:20:40 +0000

    Update to applciationlogic

--- Java_Code/DanceManagerStudioClient/src/ApplicationLayer/ApplicationLogic.java
+++ Java_Code/DanceManagerStudioClient/src/ApplicationLayer/ApplicationLogic.java
@@ -28,13 +28,25 @@ public class ApplicationLogic {
         CurrentUserSingleton.logOut();
     }
     
+<<<<<<< Updated upstream
     public void registerStudent(String uName /*,  String firstName, String lastName*/ ){
+=======
+    public int registerStudent(String userType, String uName /*,  String firstName, String lastName*/ ){
+>>>>>>> Stashed changes
         Data data = new Data();
         data.setDataName("Student");
         data.getData().add(new ArrayList<>());
         data.getData().get(0).add(uName);
         
+<<<<<<< Updated upstream
         String instruction = "";
         model.dbWrite(instruction, data);
+=======
+        model.dbWrite(userType, data);
+        // return 1 if unsuccessful 
+        //return 0 if successful
+        
+        return 1;
+>>>>>>> Stashed changes
     }
 }
