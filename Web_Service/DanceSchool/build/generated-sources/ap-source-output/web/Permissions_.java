c400bb53b5ad43df7a33ccafae0853d8edc3a567
Darragh17239567 <36898860+Darragh17239567@users.noreply.github.com>
2020-12-12 19:45:00 +0000

    # working on login info

--- Web_Service/DanceSchool/build/generated-sources/ap-source-output/web/Permissions_.java
+++ Web_Service/DanceSchool/build/generated-sources/ap-source-output/web/Permissions_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import web.Users;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-12T01:50:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-12T19:39:44")
 @StaticMetamodel(Permissions.class)
 public class Permissions_ { 
 
