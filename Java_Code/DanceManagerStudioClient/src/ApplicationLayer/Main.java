b69d67622df5187e8843f4030795fcb84dee84fe
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2020-12-12 13:37:22 +0000

    Update Main.java

--- Java_Code/DanceManagerStudioClient/src/ApplicationLayer/Main.java
+++ Java_Code/DanceManagerStudioClient/src/ApplicationLayer/Main.java
@@ -9,20 +9,14 @@ import ModelLayer.ApplicationModel;
 import dancemanagerstudioclient.DanceManagerStudioClient;
 import java.io.IOException;
 
-/**
- *
- * @author Jono
- */
 public class Main {
-            /**
+     /**
      * @param args the command line arguments
      * Main method:
-     *      Displays the welcome screen of the game
-     *      Allows you to select if you want to login or register.
+     *      
      */
     public static void main(String[] args) throws IOException {
 
-        /// will need to pass model to logic
         ApplicationLogic logic = new ApplicationLogic();
         /// will need to pass logic to view layer
         DanceManagerStudioClient myInstance = new DanceManagerStudioClient(logic);
